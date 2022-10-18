d624bc0799357f1e2f0c578cc87f7b68e0526c0d
SyauqiAlfath <99640194+SyauqiAlfath@users.noreply.github.com>
2022-10-19 07:09:38 +0800

    Add files via upload

---
+++ src/PRAK201_2110817210020_MuhammadSyauqiAlfath/Main.java
@@ -0,0 +1,18 @@
+
+package PRAK201_2110817210020_MuhammadSyauqiAlfath;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //
+        Mangga Arumanis = new Mangga("Arumanis", 0.3f, 13, 5000);
+        Mangga Manalagi = new Mangga("Manalagi", 0.5f, 17, 7500);
+        Mangga Madu     = new Mangga("Mangga Madu", 0.375f, 12, 6500);
+        // 
+        Arumanis.warung();
+        Manalagi.warung();
+        Madu.warung();
+    }
+    
+}
